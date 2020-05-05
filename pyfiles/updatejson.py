2c423cfdc80b5fcdfebd3eed109b118843d352e2
Rajdeep Biswas <32306614+therajdeepbiswas@users.noreply.github.com>
2020-05-05 10:28:42 +0530

    Corrected filepath

--- pyfiles/updatejson.py
+++ pyfiles/updatejson.py
@@ -60,7 +60,7 @@ for item in [{"date": date, "dailyconfirmed": confirmed} for [date, confirmed] i
 jsondata = str({"cases_time_series" : jsondata}).replace("'", '"')
 #print(y_pred)
 
-with open("../jsons/current.json", 'w') as f:
+with open("jsons/current.json", 'w') as f:
     f.write(json.dumps(json.loads(jsondata), indent=2, sort_keys=True))
 
-shutil.copyfile("../jsons/current.json", "../jsons/" + pdate + ".json")
+shutil.copyfile("jsons/current.json", "jsons/" + pdate + ".json")
