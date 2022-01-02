aaa1e0f5b4f5a360062433b7502dbd91854b3717
Biswas <rajdeep.biswas@sap.com>
2022-01-02 20:16:51 +0530

    refactored entire repo, updated readme

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
-# covid19-prediction
-For now, I am trying to testing what works for individual predictions if a person may or may not have COVID19.
+# COVID-19 India Daily Cases Prediction
+**Update**: https://data.covid19india.org/data.json has stopped posting updates since 2021-08-16 (16th August, 2021) and hence the GitHub Actions Workflow has ceased to update this repository on its own as a result. Sorry for any inconvenience :(  
+  
+I will try to look into some alternatives as soon as I find some free time. *Thank you for visiting!*
 
-[View a live graph here](https://araon.github.io/Covid19-Dasboard/)  
+## What does this do?
+A simple Linear Regression model with a Polynomial features of degree 4 learn the data of last 30 days of growth of COVID-19 number of cases (cumulative) and try to predict what the following 15 days would look like.  
+  
+[View a live graph here](https://araon.github.io/Covid19-Dasboard/).  
+  
+⭐ The cool thing about this project - there is a GitHub Actions Workflow [configured](https://github.com/rajdeep-biswas/covid19-prediction/blob/master/.github/workflows/python-run.yaml)  in this repository that runs [`updatejson.py`](https://github.com/rajdeep-biswas/covid19-prediction/blob/master/pyfiles/updatejson.py) runs twice everyday to get the updated number of cases from a [Public Datasource API](https://data.covid19india.org/data.json), run the forecasting for the next 15 days and write it into a JSON file that gets committed back into this repo. And that is how the above webpage gets updated everyday without requiring any backend. ;)  
+  
+There is a bunch of other code and files on this repo that are not directly related to this usecase. I have only left them here due to preservation reasons. *Delve at your own risk*!
--- src/updatejson.py
+++ src/updatejson.py
@@ -1,38 +1,40 @@
-# confirmed - recovered - deaths is a better idea since i.e the active number of cases
-
 import requests
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import json
 import shutil
+import datetime
 from random import random
 
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import LinearRegression
 
+# Requets the JSON API and fetches the most recent data
 response = requests.get('https://api.covid19india.org/data.json')
-
-# added test comment 2
 
 cases = []
 dates = []
 
+# Populates the two lists with timestamps and number of cumulative cases
 for item in response.json()["cases_time_series"]:
     cases.append(int(item["totalconfirmed"]))
     dates.append(item["date"].strip())
 
 filesavename = item["dateymd"]
 
+# Looks at a third of the most recent cases for more accurate predictions
 npastdates = len(dates) // 3
 nfuturedates = 14
 
 pdate = dates[-1]
 dates = dates[:-1]
 
-for date in pd.date_range(pdate + " 2020", periods=nfuturedates, freq='d'):
+# Adds future dates for the output JSON.
+for date in pd.date_range(pdate + str(datetime.now().year), periods=nfuturedates, freq='d'):
     dates.append(str(date.day) + " " + str(date.month_name()))
 
+# Prepares X and y for prediction.
 realcases = cases
 cases = cases[-npastdates:]
 days = list(range(len(cases)))
@@ -43,11 +45,15 @@ y = cases
 X_pred = list(range(len(cases) + nfuturedates))
 X_pred = [[x_pred] for x_pred in X_pred]
 
+# Creates model and predicts following n day cases.
+
 poly = PolynomialFeatures(4)
 
 reg = LinearRegression().fit(poly.fit_transform(X), y)
 
 y_pred = reg.predict(poly.fit_transform(X_pred))
+
+# Prepares lists for writing into updates JSON file.
 
 allcases = realcases
 for item in y_pred[-nfuturedates:]:
@@ -63,6 +69,8 @@ for item in [{"date": date, "totalconfirmed": confirmed} for [date, confirmed] i
 
 jsondata = str({"cases_time_series" : jsondata}).replace("'", '"')
 #print(y_pred)
+
+# Writes into JSON file and creates a copy for backup.
 
 with open("jsons/current.json", 'w') as f:
     f.write(json.dumps(json.loads(jsondata), indent=2, sort_keys=True))
